f7af9672feae8f759ea64e880ce0e9739271c50c
iamalex33329 <iamalex33329@gmail.com>
2023-05-18 17:23:12 +0800

    May 18, 2023

---
+++ src/Problem230.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ *  230. Kth Smallest Element in a BST
+ *
+ *  Given the root of a binary search tree, and an integer
+ *  k, return the kth smallest value (1-indexed) of all the
+ *  values of the nodes in the tree.
+ *
+ */
+
+public class Problem230 {
+
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list, k);
+        return list.get(k - 1);
+    }
+
+    private void inorder(TreeNode root, List<Integer> list, int k) {
+        if (list.size() == k) return;
+        if (root == null) return;
+
+        inorder(root.left, list, k);
+        list.add(root.val);
+        inorder(root.right, list, k);
+    }
+}
