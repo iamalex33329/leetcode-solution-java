3e3354b9ded53e1e27b8a91b54b996be10c13055
Alex Tsai <iamalex33329@gmail.com>
2024-03-22 01:27:22 +0800

    Mar 22, 2024

--- src/Problem264.java
+++ src/Problem264.java
@@ -1,3 +1,13 @@
+/**
+ *  264. Ugly Number II
+ *
+ *  An ugly number is a positive integer whose prime factors
+ *  are limited to 2, 3, and 5.
+ *
+ *  Given an integer n, return the nth ugly number.
+ *
+ */
+
 public class Problem264 {
 
     public int nthUglyNumber(int n) {
